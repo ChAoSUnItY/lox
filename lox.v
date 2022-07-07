47532803d3b33f644680aefc7be9ee07e909737d
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-07-08 00:42:02 +0800

    add basic repl

--- lox.v
+++ lox.v
@@ -1,9 +1,34 @@
 module main
 
 import os
+import readline
 
 fn main() {
-	if os.args.len < 2 {
+	if os.args.len > 2 {
 		panic('Usage: lox [script] / lox.exe [script]')
+	} else if os.args.len == 1 {
+		run_prompt()?
+	} else {
+		run_file(os.args[1])?
 	}
+}
+
+fn run_file(path string) ? {
+	source := os.read_file(path)?
+	run(source)
+}
+
+fn run_prompt() ? {
+	for {
+		line := readline.read_line('> ')?.trim_space()
+
+		if line.len == 0 {
+			break
+		}
+
+		run(line)
+	}
+}
+
+fn run(source string) {
 }
