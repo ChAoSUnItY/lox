27e35ca7afbab269e5886930cd132a3910e519e6
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-07-09 00:59:25 +0800

    migrate proc

--- lox.v
+++ lox.v
@@ -1,34 +1,16 @@
 module main
 
 import os
-import readline
+import lox
 
 fn main() {
+	mut proc := lox.Proc{}
+
 	if os.args.len > 2 {
 		panic('Usage: lox [script] / lox.exe [script]')
 	} else if os.args.len == 1 {
-		run_prompt()?
+		proc.run_prompt()?
 	} else {
-		run_file(os.args[1])?
+		proc.run_file(os.args[1])?
 	}
-}
-
-fn run_file(path string) ? {
-	source := os.read_file(path)?
-	run(source)
-}
-
-fn run_prompt() ? {
-	for {
-		line := readline.read_line('> ')?.trim_space()
-
-		if line.len == 0 {
-			break
-		}
-
-		run(line)
-	}
-}
-
-fn run(source string) {
 }
