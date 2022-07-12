988e5d5f039621a56a2f6f728a4fe88b85d0b786
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-07-13 04:23:23 +0800

    implement visitor pattern

--- ast_gen.v
+++ ast_gen.v
@@ -24,14 +24,28 @@ fn main() {
 fn define_ast(output_dir string, base_name string, types []string) ? {
 	path := '$output_dir/${base_name}.v'
 	mut file := os.open_file(path, 'w+')?
+	splitted_datas := types.map(it.split(':')).map(it.map(it.trim_space()))
 
 	file.writeln('module lox')?
 	file.writeln('')?
-	file.writeln('struct $base_name {')?
-	file.writeln('}')?
+	file.writeln('type $base_name =')?
 
-	for typ in types {
-		splitted_data := typ.split(':')
+	for i, splitted_data in splitted_datas {
+		struct_name := splitted_data[0]
+
+		file.write_string('\t')?
+
+		if i > 0 {
+			file.write_string('|')?
+		}
+
+		file.writeln('$struct_name')?
+	}
+
+	file.writeln('')?
+	define_visitor(mut file, base_name, types)?
+
+	for splitted_data in splitted_datas {
 		struct_name := splitted_data[0].trim_space()
 		fields := splitted_data[1].trim_space()
 
@@ -39,12 +53,14 @@ fn define_ast(output_dir string, base_name string, types []string) ? {
 		define_type(mut file, base_name, struct_name, fields)?
 	}
 
+	// accept function
+	file.writeln('')?
+
 	file.close()
 }
 
 fn define_type(mut file os.File, base_name string, struct_name string, field_list string) ? {
 	file.writeln('struct $struct_name {')?
-	file.writeln('\t$base_name')?
 
 	fields := field_list.split(', ')
 	for field in fields {
@@ -52,4 +68,31 @@ fn define_type(mut file os.File, base_name string, struct_name string, field_lis
 	}
 
 	file.writeln('}')?
+	file.writeln('')?
+	file.writeln('fn (mut expr $struct_name) accept<R>(visitor Visitor<R>) R {')?
+	file.writeln('\treturn visitor.visit${camel2snake(struct_name)}_${base_name.to_lower()}(expr)')?
+	file.writeln('}')?
+}
+
+fn define_visitor(mut file os.File, base_name string, types []string) ? {
+	file.writeln('interface Visitor<R> {')?
+	file.writeln('}')?
+
+	for typ in types {
+		file.writeln('')?
+		type_name := typ.split(':')[0].trim_space()
+		file.writeln('fn (mut v Visitor<R>) visit${camel2snake(type_name)}_$base_name.to_lower()<R>($base_name.to_lower() $type_name) {')? // mut?
+		file.writeln('}')?
+	}
+}
+
+fn camel2snake(id string) string {
+	return id.runes().map(fn (r rune) string {
+		s := r.str()
+		return if s.is_capital() {
+			'_$s.to_lower()'
+		} else {
+			s
+		}
+	}).join('')
 }
--- lox/expr.v
+++ lox/expr.v
@@ -1,27 +1,53 @@
 module lox
 
-struct Expr {
+type Expr = Binary | Grouping | Literal | Unary
+
+interface Visitor<R> {
+}
+
+fn (mut v Visitor<R>) visit_binary_expr<R>(expr Binary) {
+}
+
+fn (mut v Visitor<R>) visit_grouping_expr<R>(expr Grouping) {
+}
+
+fn (mut v Visitor<R>) visit_literal_expr<R>(expr Literal) {
+}
+
+fn (mut v Visitor<R>) visit_unary_expr<R>(expr Unary) {
 }
 
 struct Binary {
-	Expr
 	left     Expr
 	operator Token
 	right    Token
 }
 
+fn (mut expr Binary) accept<R>(visitor Visitor<R>) R {
+	return visitor.visit_binary_expr(expr)
+}
+
 struct Grouping {
-	Expr
 	expression Expr
 }
 
+fn (mut expr Grouping) accept<R>(visitor Visitor<R>) R {
+	return visitor.visit_grouping_expr(expr)
+}
+
 struct Literal {
-	Expr
 	value voidptr
 }
 
+fn (mut expr Literal) accept<R>(visitor Visitor<R>) R {
+	return visitor.visit_literal_expr(expr)
+}
+
 struct Unary {
-	Expr
 	operator Token
 	right    Expr
+}
+
+fn (mut expr Unary) accept<R>(visitor Visitor<R>) R {
+	return visitor.visit_unary_expr(expr)
 }
